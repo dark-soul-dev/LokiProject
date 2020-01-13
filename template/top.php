be5302c97c427babcdeb3e96eed45c58c1dd5181
Dark Soul Dev <ivanfilho20@gmail.com>
2020-01-13 10:07:06 -0300

    Add normalize.css to project

--- template/top.php
+++ template/top.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $this->title ?> | Site de Torrent</title>
+    <link href="<?= RES_PATH ?>css/normalize.css" rel="stylesheet">
     <link href="<?= RES_PATH ?>css/reset.css" rel="stylesheet">
     <link href="<?= RES_PATH ?>img/fav.ico" rel="shortcut icon">
 </head>
