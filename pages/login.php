9ebcec3327baa54a88259313fda114d36fc111c8
Dark Soul Dev <ivanfilho20@gmail.com>
2020-01-20 17:11:58 -0300

    colocar values nos inputs

--- pages/login.php
+++ pages/login.php
@@ -5,14 +5,22 @@
     input {
         display: block;
         margin: 1em auto;
+        text-align: center;
+        background: rgba(255,255,255,.3);
+    }
+    input::placeholder {
+        color: lightgray;
+    }
+    input[type=submit] {
+        padding: 0.5em;
     }
 </style>
 
 <h1>Acesso Restrito</h1>
 
 <form action="" method="post">
-    <input type="text" name="username" placeholder="Nome de Usuário">
-    <input type="password" name="password" placeholder="Senha">
+    <input type="text" name="username" placeholder="Nome de Usuário" autofocus autocomplete="off" value="<?= $un ?>">
+    <input type="password" name="password" placeholder="Senha" value="<?= $pw ?>">
 
     <input type="submit" value="Logar">
 </form>
