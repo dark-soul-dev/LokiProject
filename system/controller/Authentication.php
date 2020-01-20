51567907d324639c4f33e6e78ed3f9841660b83b
Dark Soul Dev <ivanfilho20@gmail.com>
2020-01-20 17:12:16 -0300

    adicionar metodos para autenticar

--- system/controller/Authentication.php
+++ system/controller/Authentication.php
@@ -2,11 +2,18 @@
 
 namespace Controller;
 
+use Database\User as TableUser;
+use Model\User;
+
 class Authentication extends Controller
 {
 
+    private $table;
+
     public function __construct()
-    {}
+    {
+        $this->table = new TableUser();
+    }
 
     public function index()
     {
@@ -15,6 +22,7 @@ class Authentication extends Controller
 
     public function login()
     {
+        $this->authenticate();
         $this->title = 'Entrar';
         $this->loadView('login');
     }
@@ -24,4 +32,18 @@ class Authentication extends Controller
         $this->title = 'Registro';
         $this->loadView('register');
     }
+
+    public function authenticate()
+    {
+        $post = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+        $un = !empty($post['username']) ? $post['username'] : '';
+        $pw = !empty($post['password']) ? $post['password'] : '';
+
+        $this->data['un'] = $un;
+        $this->data['pw'] = $pw;
+
+        $user = $this->table->getByUsername($un);
+        echo '<pre>' . var_export($user, true) . '</pre>';
+    }
+
 }
